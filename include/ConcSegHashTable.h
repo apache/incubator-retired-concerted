af37c9fe88aa45deadb829b3c4206fbbd02c864f
Nupur <nupurs@ingeniumsys.com>
2015-10-26 20:26:22 +0530

    CONCERTED-20:Add Apache header to files and change license

--- include/ConcSegHashTable.h
+++ include/ConcSegHashTable.h
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "QueueLock.h"
 
 template<class data_val_type> class ConcSegHashTable
@@ -33,7 +49,7 @@ public:
 		read_lock_queues = new QueueLock[m * n];
 		write_lock_queues = new QueueLock[m * n];
 		read_lock_queues->SetTimeOutPeriod(3.0);
-		write_lock_queues->SetTimeOutPeriod(3.0);	
+		write_lock_queues->SetTimeOutPeriod(3.0);
 	}
 
 	int HashVal(data_val_type val)
@@ -115,7 +131,7 @@ public:
 			cout<<"Lock already taken"<<endl;
 		}
 		else
-		{	
+		{
 			get_write_lock = GetWriteLock(seg_index, pos, name1);
 			if(get_write_lock == 0)
 			{
@@ -130,7 +146,7 @@ public:
 
 				return (1);
 			}
-			
+
 		}
 
 			return (0);
@@ -162,7 +178,7 @@ public:
 				last_insert_segment = i;
 				return 1;
 			}
-		
+
 		}
 
 		for(i = 0;i < k;i++)
--- include/QueueLock.h
+++ include/QueueLock.h
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "ConcQueue.h"
 #include <ctime>
 
@@ -70,7 +86,7 @@ public:
 		const QueueElement<int> *p_add = lock_queue.AddElementInFront(0);
 		const int *p_spin = p_add->GetPointerToData();
 		struct timespec requestStart, requestEnd;
-			
+
 		if(lock_taken != 0)
 		{
 			clock_gettime(CLOCK_REALTIME, &requestStart);
@@ -99,7 +115,7 @@ public:
 		return (1);
 	}
 
-	 
+
 	int CheckLockIsAcquired()
 	{
 
@@ -118,5 +134,3 @@ public:
 	}
 
 };
-
-			
